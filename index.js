d02c5b821c455bf2d97de1d6f535682d9005f8e8
justinravenwolfe <147559260+justinravenwolfe@users.noreply.github.com>
2024-06-04 21:20:57 -0400

    created array changes, made changed in both html and java script; linked the java script, made slide show container, added another picture of esme , created picture array and caption array.

---
+++ index.js
@@ -0,0 +1,7 @@
+//picture array
+var picArray=["photos/mollyasolaf.jpeg","photos/esmebunnypillow.JPG","photos/esmeimage.jpg"]
+//Caption array
+var capArray=["Molly sporting her olof attire for winter","Esme relaxing with her Easter pillow","esme sad about her crate"];
+//global variables
+
+//loop controll
