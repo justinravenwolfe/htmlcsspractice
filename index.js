416fa14c3c7a194642e9301982b0e4ae23f058db
justinravenwolfe <147559260+justinravenwolfe@users.noreply.github.com>
2024-06-16 16:23:04 -0400

    fixed the css specifically the sizing of the slideshow, definded global variables in the js. Defined two functions; one that edits the slideshow and one that controls the movements of the slides. Made captions larger and centered the slideshow using the center tags

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 //picture array
+document.addEventListener('DOMContentLoaded', () => {
 var picArray=["photos/mollyasolaf.jpeg","photos/esmebunnypillow.JPG","photos/esmeimage.jpg"]
 //Caption array
 var capArray=["Molly sporting her olof attire for winter","Esme relaxing with her Easter pillow","esme sad about her crate"];
 //global variables
+var image=document.getElementById("slide-image");
+var text=document.getElementById("slide-text");
+
+var currIndex=0;
+//html editing function
+function slideEditor(index){
+  image.src=picArray[index];
+  text.textContent=capArray[index];
+  
+}
 
 //loop controll
+function startSlideshow()
+ { console.log('Starting slideshow'); 
+  console.log(currIndex);
+  setInterval(() => { slideEditor(currIndex); 
+    currIndex++; 
+    if (currIndex === picArray.length)
+       { currIndex = 0; } 
+  }, 5000); // Change image every 5 seconds } startSlideshow()
+}
+startSlideshow();
+});
